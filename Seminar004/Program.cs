e0186527308e8d6b71c28dd978c023fd0b781b92
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-09-30 21:49:39 +0300

    add Seminar 004 - Range

---
+++ Seminar004/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = 0;
+if (x > 0)
+{
+    y = x * -1;
+    while (y <= x)
+    {
+        Console.WriteLine(y);
+        y = y + 1;
+    }
+}
+else
+{
+    y = x * -1;
+    while (x <= y)
+    {
+        Console.WriteLine(x);
+        x = x + 1;
+    }
+}
