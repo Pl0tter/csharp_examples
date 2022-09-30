92d169d18a94ce027e6a2aebe6ff7b5e7164ed60
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-09-30 21:04:20 +0300

    add Seminar 001 - Concatenation

---
+++ Seminar001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Hello, World!");
+string name = "Vladimir!";
+Console.WriteLine("Hello, " + name); // Метод конкатенации. Совмещение переменных
+Console.Write($"Введите число, {name}: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вы набрали: {num}");
