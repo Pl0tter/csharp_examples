e52909f2f9d0c2728a4bdf831a6be0cbc702fdf7
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-01 17:47:38 +0300

    rename Seminar 001 - Concatenation and Interpolation of Strings. Modify cs file

---
+++ Seminar001_ConcInterpStrings/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Hello, World!");
+string name = "Vladimir!";
+Console.WriteLine("Hello, " + name); // Метод конкатенации строки (склеивания). Совмещение переменных через +
+Console.Write($"Введите число, {name}: "); // Метод интерполяции строки. Совмещение переменных через $ и {}
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вы набрали: {num}");
