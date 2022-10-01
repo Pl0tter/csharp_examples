65150496b7e76f486e6718d8d6f034f0056e3451
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-01 16:07:33 +0300

    add Seminar 002 - Square Cube

---
+++ Seminar002_SquareCube/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Квадрат или куб: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (y == 2)
+{
+    x = x * x;
+    Console.WriteLine("Квадрат числа: " + x);
+}
+else
+{
+    x = x * x * x;
+    Console.WriteLine("Куб числа: " + x);
+}
