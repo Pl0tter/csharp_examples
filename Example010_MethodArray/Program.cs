b27f5d2006b0665b70f009e1fe9f4fddcfb68f3d
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-02 15:18:49 +0300

    add Example 010 - Method to Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length; // определение длины массива
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //прерывание цикла, чтобы не искать второй одинаковый элемент
+    }
+    // index = index + 1; - тоже самое что ниже
+    index++;
+}
