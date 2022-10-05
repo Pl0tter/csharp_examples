465bb15776bb398397c5bbf66253595e7e948135
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-05 12:32:42 +0300

    add Seminar 013 - Is One Number Square of Another

---
+++ Seminar013_SquareOrNot/Program.cs
@@ -0,0 +1,20 @@
+﻿// **Задача 16:**
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b)
+{
+    Console.WriteLine(a + " в квадрате равно " + b);
+}
+else if (b * b == a)
+{
+    Console.WriteLine(b + " в квадрате равно " + a);
+}
+else
+{
+    Console.WriteLine(a + " и " + b + " не являются квадратом друг друга");
+}
