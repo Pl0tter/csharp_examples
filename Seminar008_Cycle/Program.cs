17a7248f67114a433b694dcf44fb85e7dfb2ec27
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-05 10:25:51 +0300

    add Seminar 008 - Cycle While and For

---
+++ Seminar008_Cycle/Program.cs
@@ -0,0 +1,16 @@
+﻿// Условные и счётные циклы
+int age = 10;
+//string gender = "male";
+
+// Условный цикл
+while (age < 15)
+{
+    Console.WriteLine("Happy birthday, dude! You are " + age + " years old!");
+    age++;
+}
+
+// Счетный цикл
+for (int i = 1; i < 5; i++)
+{
+    Console.WriteLine("It is your " + i + " birthday!");
+}
