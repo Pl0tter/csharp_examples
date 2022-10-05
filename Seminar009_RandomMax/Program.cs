a9a6bfcf59245c4be752b25d326579a6dc562660
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-05 10:31:47 +0300

    add Seminar 009 - Random With Find Of Max Digit

---
+++ Seminar009_RandomMax/Program.cs
@@ -0,0 +1,19 @@
+﻿// Определить в случайном двухзначном числе какая из цифр больше
+int number = new Random().Next(10, 100);
+Console.WriteLine("Ваше случайное число " + number);
+int firstDigit = number % 10;
+int secondDigit = number / 10;
+Console.WriteLine("В нём " + firstDigit + " единиц и " + secondDigit + " десяток");
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit + " больше " + secondDigit);
+}
+else if (firstDigit < secondDigit)
+{
+    Console.WriteLine(firstDigit + " меньше " + secondDigit);
+}
+else
+{
+    Console.WriteLine(firstDigit + " равно " + secondDigit);
+}
