80d9c7fe77131961ea71a2c383789f72bbc45ae3
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-05 12:26:14 +0300

    add Seminar 010 - Delete Second Digit in 3 Digit Number

---
+++ Seminar010_DelSecondDigit/Program.cs
@@ -0,0 +1,17 @@
+﻿// *Задача 11:**
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine("Ваше случайное число: " + number);
+int firstDigit = number % 10;
+int secondDigit = number / 10 % 10;
+int thirdDigit = number / 100;
+Console.WriteLine("В нём " + firstDigit + " единиц, " + secondDigit + " десяток, " + thirdDigit + " сотен");
+
+int newnumber = thirdDigit * 10 + firstDigit;
+
+Console.WriteLine("Ваше конечное число: " + newnumber);
+Console.WriteLine($"Ваше конечное число: {thirdDigit}{firstDigit}"); // Вариант с интерполяцией строк
