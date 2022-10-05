722e87335f25998f8c6762ac18c8e703ff44b735
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-05 10:23:26 +0300

    add Seminar 07 - Cascade Conditions and Logic Connectives && ||

---
+++ Seminar007_CascadeIf/Program.cs
@@ -0,0 +1,26 @@
+﻿// Каскадные условия
+int age = 10;
+string gender = "male";
+
+if (age >= 20)
+{
+    Console.WriteLine("Hello, old!");
+}
+else if (age >= 10)
+{
+    Console.WriteLine("Hello, young!");
+}
+else
+{
+    Console.WriteLine("Hello, baby!");
+}
+
+if (age < 20 && gender == "male") // Условие && (and) когда выполняются оба условия
+{
+    Console.WriteLine("Hello, dude!");
+}
+
+if (age < 10 || gender == "female") // Условие || (or) когда выполняются хотя бы одно условие
+{
+    Console.WriteLine("Hello, girl!");
+}
