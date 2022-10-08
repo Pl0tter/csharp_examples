628a7c4c3a4d935357fb2e3e0592fd4e6e5aaeb8
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-08 10:08:58 +0300

    add Seminar 017 - Distance Between Dots

---
+++ Seminar017_DistanceDots/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.Write("Введите x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+double lenght = Math.Round(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)), 2); // Квадратный корень и возведение в степень
+Console.WriteLine("Растояние между точками равно: " + lenght);
