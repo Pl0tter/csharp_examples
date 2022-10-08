f92ee49f3dbdba9135fedb5cb2b4db81cefcdf0c
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-08 10:06:48 +0300

    add Seminar 016 - Coordinates Range in Quadrants

---
+++ Seminar016_CoordinatesRange/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите четверть декартовой ситемы координат: ");
+int quad = Convert.ToInt32(Console.ReadLine());
+if (quad < 1 || quad > 4) Console.WriteLine("Такой четверти не существует");
+if (quad == 1) Console.WriteLine("Диапазон координат от (0; 0) до (" + int.MaxValue + "; " + int.MaxValue + ")");
+if (quad == 2) Console.WriteLine("Диапазон координат от (0; 0) до (" + -int.MaxValue + "; " + int.MaxValue + ")");
+if (quad == 3) Console.WriteLine("Диапазон координат от (0; 0) до (" + -int.MaxValue + "; " + -int.MaxValue + ")");
+if (quad == 4) Console.WriteLine("Диапазон координат от (0; 0) до (" + int.MaxValue + "; " + -int.MaxValue + ")");
