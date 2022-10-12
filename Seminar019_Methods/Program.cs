6a30a0bccee9ecc9ea60d8795686499ceac3b70f
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-12 12:16:45 +0300

    add Seminar 019 - Methods (3 Types)

---
+++ Seminar019_Methods/Program.cs
@@ -0,0 +1,35 @@
+﻿//====Методы====
+
+// Ничего не принимает и ничего не возвращает
+void SayHello()
+{
+    Console.WriteLine("Hello, C#!");
+}
+
+// Принимает аргументы, но ничего не возвращает 
+void SayHelloLanguage(string lang)
+{
+    Console.WriteLine($"Hello, {lang}!");
+}
+
+SayHello();
+SayHelloLanguage("Python");
+SayHelloLanguage("C++");
+// Принимает аргументы, но ничего не возвращает 
+void MinMax(int x, int y)
+{
+    if (x > y) Console.WriteLine(x);
+    else Console.WriteLine(y);
+}
+
+MinMax(3, 5);
+
+// Принимает и возвращает (return) аргументы
+int Middle(int x, int y)
+{
+    int result = (x + y) / 2;
+    return result;
+}
+
+int c = Middle(3, 5);
+Console.WriteLine(c);
