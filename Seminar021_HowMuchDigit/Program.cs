ed3af3b5325ab0197ff6d9f03c6dfa57d4da7fba
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-12 12:18:01 +0300

    add Seminar 021 - How Much Digit in Number

---
+++ Seminar021_HowMuchDigit/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int HowMuchDigit(int A)
+{
+    int count = 0;
+    while (A > 0)
+    {
+        A = A / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"В числе {A} {HowMuchDigit(A)} цифр");
