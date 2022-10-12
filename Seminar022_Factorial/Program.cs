3871af32cbf6a0dfe8e79ded430cde9740967a22
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-12 12:18:30 +0300

    add Seminar 022 - Factorial

---
+++ Seminar022_Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int Factorial(int N)
+{
+    int prod = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        prod = prod * i;
+    }
+    return prod;
+}
+
+Console.Write("Введите число :");
+int N = Convert.ToInt32(Console.ReadLine());
+int prod = Factorial(N);
+Console.WriteLine($"Факториал числа {N} равен {prod}");
