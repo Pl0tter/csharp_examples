1e34fc06750a1ee7e6b1940d406559fe29c74841
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-12 15:09:37 +0300

    add Homework 012 - Exponent

---
+++ Homework012_Exponent/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Exponent(int a, int b)
+{
+    int product = 1;
+    for (int i = 0; i < b; i++) product = product * a;
+    return product;
+}
+
+Console.Write("Введите основание степени: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите показатель степени: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Результат равен: " + Exponent(a, b));
