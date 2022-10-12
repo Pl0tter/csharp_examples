787aca2a95a91fb8e355386cd0872e9f90cff842
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-12 12:17:18 +0300

    add Seminar 020 - Sum from 1 to A

---
+++ Seminar020_Sum1toA/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+void Sum(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        sum = sum + i;
+    }
+    Console.WriteLine(sum);
+}
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Sum(A);
