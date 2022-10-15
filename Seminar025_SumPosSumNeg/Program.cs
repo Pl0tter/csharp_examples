e9754d8f21c4b781a7ae62a98408405d4fa632ac
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-15 12:48:18 +0300

    add Seminar 025 - Sums of Positive and Negative Numbers

---
+++ Seminar025_SumPosSumNeg/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+int[] array = new int[12];
+int sumPos = 0;
+int sumNeg = 0;
+Console.WriteLine("Массив из 12 элементов:");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    Console.Write(array[i] + " ");
+    if (array[i] > 0) sumPos = sumPos + array[i];
+    else sumNeg = sumNeg + array[i];
+}
+
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных элементов равна: {sumPos}");
+Console.WriteLine($"Сумма отрицательных элементов равна: {sumNeg}");
