b04f3706dd39f398789bae6f803b14c3c83e3195
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-15 19:36:04 +0300

    add Homework 016 - Sum of Numbers on Not Even Position

---
+++ Homework016_SumNotEvenPosition/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+int sum = 0;
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 11);
+    Console.Write(array[i] + " ");
+    if (i % 2 != 0) sum = sum + array[i];
+}
+Console.WriteLine();
+Console.WriteLine("Сумма элементов, стоящих на нечётных позициях: " + sum);
