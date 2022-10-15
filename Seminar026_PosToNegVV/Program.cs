3f63094087b84bdc6e18f88ac804324b1ddbff23
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-15 12:49:44 +0300

    add Seminar 026 - Convert Positive to Negative Arrays Elements and Vice Versa

---
+++ Seminar026_PosToNegVV/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+void signChange(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+Console.Write("Исходный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+Console.Write("Измененный массив: ");
+signChange(array);
