5891dc5794aa4a079c28400a685e3cff9a1b823c
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-16 10:57:46 +0300

    modify Seminar 024 - Add Quick Variant

--- Seminar024_5RootFind/Program.cs
+++ Seminar024_5RootFind/Program.cs
@@ -3,9 +3,14 @@
 // Нужно перебрать их все.
 // Подумать, как лучше это сделать
 
+// Закомментирован вариант с перебором x5.
+// Текущий вариант с извлечением корня левой части. Извлечение корня, почему-то всегда возвращает остаток, хотя его не должно быть. Поэтому проверка на остаток < 0.00000000001.
+
 ulong length = 300;
-ulong start = 0; // начало для x5
-ulong end = 0; // конец для x5
+// ulong start = 0; // начало для x5
+// ulong end = 0; // конец для x5
+ulong sum = 0;
+double sumRoot = 0;
 
 for (ulong a = 1; a <= length; a++)
 {
@@ -15,18 +20,21 @@ for (ulong a = 1; a <= length; a++)
         {
             for (ulong d = c; d <= length; d++)
             {
-                if (a > b && a > c && a > d) end = Convert.ToUInt64(a * 1.32); // a * 4^(1/5)
-                if (b > a && b > c && b > d) end = Convert.ToUInt64(b * 1.32);
-                if (c > a && c > b && c > d) end = Convert.ToUInt64(c * 1.32);
-                if (d > a && d > b && d > c) end = Convert.ToUInt64(d * 1.32);
-                start = Convert.ToUInt64(a * 1.32);
-                for (ulong e = start; e <= end; e++)
-                {
-                    if ((a * a * a * a * a) + (b * b * b * b * b) + (c * c * c * c * c) + (d * d * d * d * d) == (e * e * e * e * e))
-                    {
-                        Console.WriteLine(a + " " + b + " " + c + " " + d + " " + e);
-                    }
-                }
+                // if (a > b && a > c && a > d) end = Convert.ToUInt64(a * 1.32); // a * 4^(1/5)
+                // if (b > a && b > c && b > d) end = Convert.ToUInt64(b * 1.32);
+                // if (c > a && c > b && c > d) end = Convert.ToUInt64(c * 1.32);
+                // if (d > a && d > b && d > c) end = Convert.ToUInt64(d * 1.32);
+                // start = Convert.ToUInt64(a * 1.32);
+                // for (ulong e = start; e <= end; e++)
+                // {
+                //     if ((a * a * a * a * a) + (b * b * b * b * b) + (c * c * c * c * c) + (d * d * d * d * d) == (e * e * e * e * e))
+                //     {
+                //         Console.WriteLine(a + " " + b + " " + c + " " + d + " " + e);
+                //     }
+                // }
+                sum = (a * a * a * a * a) + (b * b * b * b * b) + (c * c * c * c * c) + (d * d * d * d * d);
+                sumRoot = Math.Pow(sum, 0.2);
+                if (sumRoot - Math.Truncate(sumRoot) < 0.00000000001) Console.WriteLine(a + " " + b + " " + c + " " + d + " " + (int)sumRoot);
             }
         }
     }
