df055c252369e083bff206c76dc98525299c71da
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-19 15:30:22 +0300

    add Homework 018 - Greater Then Zero

---
+++ Homework018-GreaterZero/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+string[] arrayText;
+int count = 0;
+Console.WriteLine("Введите числа через пробел:");
+arrayText = Console.ReadLine().Split(" ");
+
+for (int i = 0; i < arrayText.Length; i++)
+{
+    if (Convert.ToInt32(arrayText[i])>0) count++;
+}
+Console.WriteLine($"Количество чисел больше нуля: {count}");
