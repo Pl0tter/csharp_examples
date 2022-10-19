f44cbd10dd6eece57a0690176d4fa4e446209ecb
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-10-19 12:39:15 +0300

    add Seminar 035 - Copy Array Element by Element

---
+++ Seminar035_CopyArray/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+Console.Write("Введите размер массива: ");
+int l = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[l];
+int[] arrayCopy = new int[l];
+
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+Console.WriteLine("Копия массива: ");
+for (int j = 0; j < array.Length; j++)
+{
+    arrayCopy[j] = array[j];
+    Console.Write($"{arrayCopy[j]} ");
+}
+Console.WriteLine();
