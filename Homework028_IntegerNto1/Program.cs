261bd52e90479cc5d6a4806405327b91d6a586fa
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-11-13 17:30:08 +0300

    add Homework 028 - Recursion Integer N to 1

---
+++ Homework028_IntegerNto1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string RecNto1String(int Number)
+{
+    if (Number == 1) return $"{Number}";
+    else return $"{Number} " + RecNto1String(Number - 1);
+}
+
+void RecNto1Void(int Number)
+{
+    if (Number == 1) Console.WriteLine(Number);
+    else
+    {
+        Console.Write($"{Number} ");
+        RecNto1Void(Number - 1);
+    }
+}
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(RecNto1String(n));
+RecNto1Void(n);
