dcbfdd19624cb42927a3aef4b32e97beddf25cbc
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-11-13 18:53:45 +0300

    add Example 014 - Example of Using var

---
+++ Example014_Var/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq; //подключение системной библиотеки
+var a = 12; // поскольку явно заявлено целое число 12, то переменная var определяется как int
+Console.WriteLine(a.GetType()); // выводит тип переменной. GetType().Name - убираете в выводе вначале System
+
+var data = new int[] { 1, 2, 3, 4 }
+            .Where(e => e > 0)
+            .Select(e => new { q = e, w = e + 1 });
+Console.WriteLine(data.GetType().Name);
+Console.WriteLine(data.GetType());
