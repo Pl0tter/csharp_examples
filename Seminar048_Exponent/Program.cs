037f80ba9fbda833479439f6009264e16063f4c3
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-11-13 18:52:45 +0300

    add Seminar 048 - Exponentiation

---
+++ Seminar048_Exponent/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int Exp(int a, int b)
+{
+    if (b == 1) return a;
+    else return a * Exp(a, b - 1);
+}
+Console.Write("Введите основание степени A: ");
+int a = Convert.ToInt32(Console.ReadLine());;
+Console.Write("Введите показатель степени B: ");
+int b = Convert.ToInt32(Console.ReadLine());;
+Console.WriteLine(Exp(a, b));
