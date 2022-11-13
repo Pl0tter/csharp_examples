61158a5304c1ed7d5e3bf753a3eaae47c6b177a0
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-11-13 18:48:03 +0300

    add Seminar 040 - Change First and Last Rows

---
+++ Seminar040_ChangeFirstNLastRows/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) array[i, j] = new Random().Next(0, 10);
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+}
+
+void ChangeLines(int[,] array)
+{
+    int temp = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        temp = array[0, j];
+        array[0, j] = array[array.GetLength(0) - 1, j];
+        array[array.GetLength(0) - 1, j] = temp;
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChangeLines(array);
+PrintArray(array);
