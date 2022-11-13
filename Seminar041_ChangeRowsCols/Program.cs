68e9e87fa9bdb5c28feb900b4bcf0aa7af1a7919
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-11-13 18:49:00 +0300

    add Seminar 041 - Change Rows and Columns

---
+++ Seminar041_ChangeRowsCols/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) array[i, j] = new Random().Next(0, 10);
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+}
+
+void ChangeRowsCols(int[,] array)
+{
+    int temp;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0 + i; j < array.GetLength(1); j++)
+        {
+            temp = array[i, j];
+            array[i, j] = array[j, i];
+            array[j, i] = temp;
+        }
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[4, 4];
+
+if (m == n)
+{
+    FillArray(array);
+    PrintArray(array);
+    Console.WriteLine();
+    ChangeRowsCols(array);
+    PrintArray(array);
+}
+else Console.WriteLine("Поменять строки и столбы в текущем массиве не возможно");
